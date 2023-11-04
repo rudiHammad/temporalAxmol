bfebd91fa0a77dc173ab9dd85cc7037e65b10d7d
rudiHammad <rudihammad@gmail.com>
2023-11-04 18:17:54 +0100

    Add files via upload

---
+++ tiled/tiles.tsx
@@ -0,0 +1,238 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="tiles" tilewidth="32" tileheight="32" tilecount="45" columns="9">
+ <image source="../aseprite/tiles.png" width="288" height="160"/>
+ <tile id="0">
+  <properties>
+   <property name="m" type="float" value="0"/>
+   <property name="p" type="float" value="0"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="m" type="float" value="1"/>
+   <property name="p" type="float" value="0"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="m" type="float" value="-1"/>
+   <property name="p" type="float" value="1"/>
+  </properties>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="m" type="float" value="0.5"/>
+   <property name="p" type="float" value="0"/>
+  </properties>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="m" type="float" value="0.5"/>
+   <property name="p" type="float" value="0.5"/>
+  </properties>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="m" type="float" value="-0.5"/>
+   <property name="p" type="float" value="1"/>
+  </properties>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="m" type="float" value="-0.5"/>
+   <property name="p" type="float" value="0.5"/>
+  </properties>
+ </tile>
+ <tile id="7">
+  <properties>
+   <property name="m" type="float" value="1.5"/>
+   <property name="p" type="float" value="0"/>
+  </properties>
+ </tile>
+ <tile id="8">
+  <properties>
+   <property name="m" type="float" value="1.5"/>
+   <property name="p" type="float" value="-1"/>
+  </properties>
+ </tile>
+ <tile id="9">
+  <properties>
+   <property name="m" type="float" value="1.5"/>
+   <property name="p" type="float" value="0.5"/>
+  </properties>
+ </tile>
+ <tile id="10">
+  <properties>
+   <property name="m" type="float" value="1.5"/>
+   <property name="p" type="float" value="-0.5"/>
+  </properties>
+ </tile>
+ <tile id="11">
+  <properties>
+   <property name="m" type="float" value="-1.5"/>
+   <property name="p" type="float" value="1"/>
+  </properties>
+ </tile>
+ <tile id="12">
+  <properties>
+   <property name="m" type="float" value="-1.5"/>
+   <property name="p" type="float" value="2"/>
+  </properties>
+ </tile>
+ <tile id="13">
+  <properties>
+   <property name="m" type="float" value="-1.5"/>
+   <property name="p" type="float" value="0.5"/>
+  </properties>
+ </tile>
+ <tile id="14">
+  <properties>
+   <property name="m" type="float" value="-1.5"/>
+   <property name="p" type="float" value="1.5"/>
+  </properties>
+ </tile>
+ <tile id="15">
+  <properties>
+   <property name="m" type="float" value="0.125"/>
+   <property name="p" type="float" value="0"/>
+  </properties>
+ </tile>
+ <tile id="16">
+  <properties>
+   <property name="m" type="float" value="0.125"/>
+   <property name="p" type="float" value="0.125"/>
+  </properties>
+ </tile>
+ <tile id="17">
+  <properties>
+   <property name="m" type="float" value="0.125"/>
+   <property name="p" type="float" value="0.25"/>
+  </properties>
+ </tile>
+ <tile id="18">
+  <properties>
+   <property name="m" type="float" value="0.125"/>
+   <property name="p" type="float" value="0.375"/>
+  </properties>
+ </tile>
+ <tile id="19">
+  <properties>
+   <property name="m" type="float" value="0.125"/>
+   <property name="p" type="float" value="0.5"/>
+  </properties>
+ </tile>
+ <tile id="20">
+  <properties>
+   <property name="m" type="float" value="0.125"/>
+   <property name="p" type="float" value="0.625"/>
+  </properties>
+ </tile>
+ <tile id="21">
+  <properties>
+   <property name="m" type="float" value="0.125"/>
+   <property name="p" type="float" value="0.75"/>
+  </properties>
+ </tile>
+ <tile id="22">
+  <properties>
+   <property name="m" type="float" value="0.125"/>
+   <property name="p" type="float" value="0.875"/>
+  </properties>
+ </tile>
+ <tile id="23">
+  <properties>
+   <property name="m" type="float" value="-0.125"/>
+   <property name="p" type="float" value="1"/>
+  </properties>
+ </tile>
+ <tile id="24">
+  <properties>
+   <property name="m" type="float" value="-0.125"/>
+   <property name="p" type="float" value="0.875"/>
+  </properties>
+ </tile>
+ <tile id="25">
+  <properties>
+   <property name="m" type="float" value="-0.125"/>
+   <property name="p" type="float" value="0.75"/>
+  </properties>
+ </tile>
+ <tile id="26">
+  <properties>
+   <property name="m" type="float" value="-0.125"/>
+   <property name="p" type="float" value="0.625"/>
+  </properties>
+ </tile>
+ <tile id="27">
+  <properties>
+   <property name="m" type="float" value="-0.125"/>
+   <property name="p" type="float" value="0.5"/>
+  </properties>
+ </tile>
+ <tile id="28">
+  <properties>
+   <property name="m" type="float" value="-0.125"/>
+   <property name="p" type="float" value="0.375"/>
+  </properties>
+ </tile>
+ <tile id="29">
+  <properties>
+   <property name="m" type="float" value="-0.125"/>
+   <property name="p" type="float" value="0.25"/>
+  </properties>
+ </tile>
+ <tile id="30">
+  <properties>
+   <property name="m" type="float" value="-0.125"/>
+   <property name="p" type="float" value="0.125"/>
+  </properties>
+ </tile>
+ <tile id="31">
+  <properties>
+   <property name="m" type="float" value="0.25"/>
+   <property name="p" type="float" value="0"/>
+  </properties>
+ </tile>
+ <tile id="32">
+  <properties>
+   <property name="m" type="float" value="0.25"/>
+   <property name="p" type="float" value="0.25"/>
+  </properties>
+ </tile>
+ <tile id="33">
+  <properties>
+   <property name="m" type="float" value="0.25"/>
+   <property name="p" type="float" value="0.5"/>
+  </properties>
+ </tile>
+ <tile id="34">
+  <properties>
+   <property name="m" type="float" value="0.25"/>
+   <property name="p" type="float" value="0.75"/>
+  </properties>
+ </tile>
+ <tile id="35">
+  <properties>
+   <property name="m" type="float" value="-0.25"/>
+   <property name="p" type="float" value="1"/>
+  </properties>
+ </tile>
+ <tile id="36">
+  <properties>
+   <property name="m" type="float" value="-0.25"/>
+   <property name="p" type="float" value="0.75"/>
+  </properties>
+ </tile>
+ <tile id="37">
+  <properties>
+   <property name="m" type="float" value="-0.25"/>
+   <property name="p" type="float" value="0.5"/>
+  </properties>
+ </tile>
+ <tile id="38">
+  <properties>
+   <property name="m" type="float" value="-0.25"/>
+   <property name="p" type="float" value="0.25"/>
+  </properties>
+ </tile>
+</tileset>
